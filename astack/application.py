8c802032642513f1fa8202837062c91298752dda
Ian Kenney <ian.kenney@asu.edu>
2019-04-11 12:04:28 -0700

    fixed some bugs

--- astack/application.py
+++ astack/application.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tqdm
 
 def linear_application(iterator, hdf5_file, function_definitions, append=True):
     """Applicator for function definitions.
@@ -18,14 +19,15 @@ def linear_application(iterator, hdf5_file, function_definitions, append=True):
     length = len(iterator)
     locs = []
 
-    for func, params in function_def:
+    for func, params in function_definitions:
         dataset_name, dataset_dtype, function_kwargs = params
         data = np.zeros((length), dtype=dataset_dtype)
 
-        exists = dataset_name in hdf5_file:
-            if exists and append:
-                print("Appending to dataset: {0}:".format(dataset_name))
-                locs.append(hdf5_file[dataset_name].attrs["processed"])
+        exists = dataset_name in hdf5_file
+
+        if exists and append:
+            print("Appending to dataset: {0}:".format(dataset_name))
+            locs.append(hdf5_file[dataset_name].attrs["processed"])
 
             if hdf5_file[dataset_name].shape[0] < length:
                 try:
@@ -35,23 +37,29 @@ def linear_application(iterator, hdf5_file, function_definitions, append=True):
                     print("Couldn't resize dataset")
                     print(e.message)
                     raise e
-            elif exists and not append:
-                print("Writing over old dataset: {0}".format(dataset_name))
-                del hd5f_file[dataset_name]
-                locs.append(0)
-                hdf5_file.create_dataset(dataset_name, data=data,
-                                         chunks=True,
-                                         maxshape=((None,) + data.shape[1:]))
-                hdf5_file[dataset_name].attrs["processed"] = 0
+        elif exists and not append:
+            print("Writing over old dataset: {0}".format(dataset_name))
+            del hd5f_file[dataset_name]
+            locs.append(0)
+            hdf5_file.create_dataset(dataset_name, data=data,
+                                     chunks=True,
+                                     maxshape=((None,) + data.shape[1:]))
+            hdf5_file[dataset_name].attrs["processed"] = 0
+
+        else:
+            print("Creating new dataset: {0}".format(dataset_name))
+            locs.append(0)
+            hdf5_file.create_dataset(dataset_name, data=data, chunks=True, maxshape=((None,) + data.shape[1:]))
+            hdf5_file[dataset_name].attrs["processed"] = 0
 
         min_value = min(locs)
 
-        for num, i in tqdm.tqdm(enumerate(iterator[min_value:], initial=min_value, total=length)):
-            for func, params in function_def:
-                number = num + min_value
-                dataset_name, dataset_dtype, function_kwargs = params
-                dataset = hdf5_file[dataset_name]
-                if dataset.attrs["processed"] > (number):
-                    continue
-                dataset[number] = func(i, **function_kwargs)
-                dataset.attrs["processed"] = number
+    for num, i in tqdm.tqdm(enumerate(iterator[min_value:]), initial=min_value, total=length):
+        for func, params in function_definitions:
+            number = num + min_value
+            dataset_name, dataset_dtype, function_kwargs = params
+            dataset = hdf5_file[dataset_name]
+            if dataset.attrs["processed"] > (number):
+                continue
+            dataset[number] = func(i, **function_kwargs)
+            dataset.attrs["processed"] = number
