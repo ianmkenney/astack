7561592682db37b4351b93808c9ad0a7d08abbed
Ian Kenney <ian.kenney@asu.edu>
2019-04-11 11:06:27 -0700

    initial commit

---
+++ astack/application.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+def linear_application(iterator, hdf5_file, function_definitions, append=True):
+    """Applicator for function definitions.
+
+    Parameters:
+    iterator (list like): An iterator whose elements will be the target
+                          of the functions defined in the function
+                          definitions.
+    hd5f_file (hdf5.File): A hdf5 file handle that will be used to store the
+                           resulting analyses
+    function_definitions (list): A list containing tuples that define which
+                                 functions will be run on the iterable elements
+                                 while also providing a way to define the
+                                 datatype that will be used in the HDF5 dataset
+    """
+
+    length = len(iterator)
+    locs = []
+
+    for func, params in function_def:
+        dataset_name, dataset_dtype, function_kwargs = params
+        data = np.zeros((length), dtype=dataset_dtype)
+
+        exists = dataset_name in hdf5_file:
+            if exists and append:
+                print("Appending to dataset: {0}:".format(dataset_name))
+                locs.append(hdf5_file[dataset_name].attrs["processed"])
+
+            if hdf5_file[dataset_name].shape[0] < length:
+                try:
+                    print("Attempting to resize")
+                    hdf5_file[dataset_name].resize(length, axis=0)
+                except Exception as e:
+                    print("Couldn't resize dataset")
+                    print(e.message)
+                    raise e
+            elif exists and not append:
+                print("Writing over old dataset: {0}".format(dataset_name))
+                del hd5f_file[dataset_name]
+                locs.append(0)
+                hdf5_file.create_dataset(dataset_name, data=data, chunks=True, maxshape=((None,) + data.shape[1:]))
+                hdf5_file[dataset_name].attrs["processed"] = 0
+
+        min_value = min(locs)
+
+        for num, i in tqdm.tqdm(enumerate(iterator[min_value:], initial=min_value, total=length)):
+            for func, params in function_def:
+                number = num + min_value
+                dataset_name, dataset_dtype, function_kwargs = params
+                dataset = hdf5_file[dataset_name]
+                if dataset.attrs["processed"] > (number):
+                    continue
+                dataset[number] = func(i, **function_kwargs)
+                dataset.attrs["processed"] = number
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setuptools
+
+setup(name="astack",
+      version="0.1",
+      author="Ian Kenney",
+      author_email="ianmichaelkenney@gmail.com",
+      packages=["astack"],
+      install_requires=["numpy",
+                        "h5py",
+                        "tqdm"]
+      )
